48020ef122270201845cde6d7b796420fc1395e8
Andrii Bobryshev <andrii.bobryshev@deliveroo.co.uk>
2022-01-14 11:20:56 +0200

    Unit test example

--- wallet/go.mod
+++ wallet/go.mod
@@ -1,3 +1,8 @@
 module github.com/workshops/wallet
 
 go 1.17
+
+require (
+	github.com/golang/mock v1.6.0
+	github.com/stretchr/testify v1.7.0
+)
--- wallet/internal/services/wallet/service.go
+++ wallet/internal/services/wallet/service.go
@@ -1,6 +1,8 @@
 package wallet
 
 type Repository interface {
+	GetBalance(walletID string) (int64, error)
+	GetCreditLimit(walletID string) (int64, error)
 }
 
 // Service holds calendar business logic and works with repository
@@ -10,4 +12,19 @@ type Service struct {
 
 func NewService(repo Repository) *Service {
 	return &Service{repo: repo}
+}
+
+// GetPaymentCapacity Returns sum of wallet balance and credit allowance
+func (s *Service) GetPaymentCapacity(walletID string) (int64, error) {
+	balance, err := s.repo.GetBalance(walletID)
+	if err != nil {
+		return 0, err
+	}
+
+	credit, err := s.repo.GetCreditLimit(walletID)
+	if err != nil {
+		return 0, err
+	}
+
+	return balance + credit, nil
 }
